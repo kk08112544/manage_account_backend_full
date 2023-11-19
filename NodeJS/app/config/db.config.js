cb56a0adc6f3e3b3a370e259432b4f05da46bb7f
Your Name <s6330613008@phuket.psu.ac.th>
2023-11-19 21:23:35 +0700

    first commit

---
+++ NodeJS/app/config/db.config.js
@@ -0,0 +1,10 @@
+module.exports ={
+    // HOST: "localhost",
+    // USER: "root",
+    // PASSWORD: "",
+    // DB: "myapi"
+    HOST: "bpnnonfhudahfxwguxxj-mysql.services.clever-cloud.com",
+    USER: "um5mtqbcjwcixuip",
+    PASSWORD: "rqkA3pBlG5oG7Sy75HRU",
+    DB: "bpnnonfhudahfxwguxxj"
+};
